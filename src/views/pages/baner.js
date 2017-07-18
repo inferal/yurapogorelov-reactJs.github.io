253d1e234b1a6b0ded8d8be1103808caa2184f9e
DESKTOP-RHPFIMF\yurik <pyurik44@gmail.com>
2017-07-18 21:01:02 +0300

    test

---
+++ src/views/pages/baner.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Baner extends Component {
+    render(){
+        return(
+            <h1>Банер</h1>
+        )
+    }
+}
+
+export default Baner;
