80aa68f8385f63862641d06df130cd2ece59507c
DESKTOP-RHPFIMF\yurik <pyurik44@gmail.com>
2017-07-19 00:28:42 +0300

    header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 import Header from './views/Header';
 import Footer from './views/Footer';
+import './index.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="main">
           <Header/>
           <Footer/>
       </div>
--- src/views/Header.js
+++ src/views/Header.js
@@ -1,35 +1,31 @@
 import React, { Component } from 'react'
 
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-import Baner from './pages/baner';
+//import Baner from './pages/baner';
+
+import '../css/header.css'
 
 class Header extends Component{
 
     render(){
         return(
-            <div>
-                <Navbar inverse collapseOnSelect>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <a href="">React-Bootstrap</a>
-                        </Navbar.Brand>
-                        <Navbar.Toggle />
-                    </Navbar.Header>
-                    <Navbar.Collapse>
-                        <Nav pullRight>
-                            <NavItem eventKey={1} href="#">Home</NavItem>
-                            <NavItem eventKey={2} href="#">About</NavItem>
-                            <NavItem eventKey={3} href="#">Service</NavItem>
-                            <NavItem eventKey={4} href="#">Work</NavItem>
-                            <NavItem eventKey={5} href="#">Testimonial</NavItem>
-                            <NavItem eventKey={6} href="#">Blog</NavItem>
-                            <NavItem eventKey={7} href="#">Team</NavItem>
-                            <NavItem eventKey={8} href="#">Contact</NavItem>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-                <Baner />
+            <div className="header">
+                <div className="logo">
+                    <img src="../img/logo.jpg" alt=""/>
+                    <h1>the<span>Ham</span></h1>
+                </div>
+                <div className="menu">
+                    <ul>
+                        <li><a href="" className="menu-link">Home</a></li>
+                        <li><a href="" className="menu-link">About</a></li>
+                        <li><a href="" className="menu-link">Service</a></li>
+                        <li><a href="" className="menu-link">Work</a></li>
+                        <li><a href="" className="menu-link">Testimonial</a></li>
+                        <li><a href="" className="menu-link">Blog</a></li>
+                        <li><a href="" className="menu-link">Team</a></li>
+                        <li><a href="" className="menu-link">Contact</a></li>
+                    </ul>
+                </div>
             </div>
         )
     }
